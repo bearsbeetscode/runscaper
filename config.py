22111b14ffe0010ff0be4b79e5e329c9e531f4ef
Arvis Upenieks <arvisupenieks@gmail.com>
2026-01-25 07:20:46 +0200

    Created secret file

--- config.py
+++ config.py
@@ -1,14 +1,21 @@
 from pathlib import Path
 
+
 # Paths
 STRAVA_DIR = Path.home() / "Documents/GPXFiles"
 OUTPUT_IMAGE = Path.home() / "Pictures/runscaper/running_heatmap.png"
 
 # Colors
-BG_COLOR = "#2E3440"
-TXT_COLOR = "#ECEFF4"
-DAY_COLOR = "#88C0D0"
-NIGHT_COLOR = "#B48EAD"
+BG_COLOR = "#232136"
+TXT_COLOR = "#E0DEF4"
+DAY_COLOR = "#9CCFD8"
+NIGHT_COLOR = "#EBBCBA"
 
-HOME_LAT = 56.916
-HOME_LON = 24.187
+try:
+    import my_secrets
+
+    HOME_LAT = my_secrets.HOME_LAT
+    HOME_LON = my_secrets.HOME_LON
+except ImportError:
+    HOME_LAT = 37.82926344244314
+    HOME_LON = -92.10512707843111
