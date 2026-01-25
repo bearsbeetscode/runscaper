6f6502cca97700b0f8b3c860d0726c8e1f6fd616
Arvis Upenieks <arvisupenieks@gmail.com>
2026-01-25 08:03:00 +0200

    Update Sun Jan 25 08:03:00 EET 2026

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ STRAVA_DIR = Path.home() / "Documents/GPXFiles"
 OUTPUT_IMAGE = Path.home() / "Pictures/runscaper/running_heatmap.png"
 
 # Colors
-BG_COLOR = "#232136"
-TXT_COLOR = "#E0DEF4"
-DAY_COLOR = "#9CCFD8"
-NIGHT_COLOR = "#EBBCBA"
+BG_COLOR = "#2E3440"
+TXT_COLOR = "#ECEFF4"
+DAY_COLOR = "#88C0D0"
+NIGHT_COLOR = "#B48EAD"
 
 try:
     import my_secrets
